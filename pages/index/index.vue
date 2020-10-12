b67fcd41d6ecfd465cfe68d859a7fd29d66da7c7
jixindong <2010342518@qq.com>
2020-10-12 09:44:10 +0800

    add

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -53,11 +53,11 @@
 				<view class="title">{{ item.title }}</view>
 				<view class="accessory">
 					<image src="../../static/images/rose.png" mode="widthFix"></image>
-					<text>{{item.accessoryName}}.{{item.accessoryType}}</text>
+					<text>{{ item.accessoryName }}.{{ item.accessoryType }}</text>
 				</view>
 				<view class="readMsg">
 					<image src="../../static/images/rose.png" mode="widthFix"></image>
-					<text>{{item.num}}万人围观</text>
+					<text>{{ item.num }}万人围观</text>
 				</view>
 			</view>
 		</view>
@@ -72,9 +72,68 @@ export default {
 			// banner
 			bannerList: ['https://s1.ax1x.com/2020/10/11/0cObr9.jpg', 'https://s1.ax1x.com/2020/10/11/0cObr9.jpg'],
 			// 最热分享
-			fieryShareList: [],
+			fieryShareList: [
+				{
+					id: 1,
+					poster: 'https://s1.ax1x.com/2020/10/11/0cxdsg.jpg',
+					title: '李白一斗诗百篇，长安市上酒家眠。天子呼来不上船，自称臣是酒中仙。',
+					userAvatar: 'https://s1.ax1x.com/2020/10/11/0cxpxU.jpg',
+					userName: '法外狂徒张三',
+					num: 66.66
+				},
+				{
+					id: 2,
+					poster: 'https://s1.ax1x.com/2020/10/11/0cxdsg.jpg',
+					title: '李白一斗诗百篇，长安市上酒家眠。天子呼来不上船，自称臣是酒中仙。',
+					userAvatar: 'https://s1.ax1x.com/2020/10/11/0cxpxU.jpg',
+					userName: '爱美妆的木子萌爱美妆的木子萌',
+					num: 23.33
+				},
+				{
+					id: 3,
+					poster: 'https://s1.ax1x.com/2020/10/11/0cxdsg.jpg',
+					title: '李白一斗诗百篇，长安市上酒家眠。天子呼来不上船，自称臣是酒中仙。',
+					userAvatar: 'https://s1.ax1x.com/2020/10/11/0cxpxU.jpg',
+					userName: '法外狂徒张三',
+					num: 88.88
+				}
+			],
 			// 最新分享
-			newShareList: []
+			newShareList: [
+				{
+					id: 1,
+					userAvatar: 'https://s1.ax1x.com/2020/10/11/0cxdsg.jpg',
+					userName: '杜甫',
+					title: '《饮中八仙歌》',
+					accessoryName: '饮中八仙歌',
+					accessoryLink: 'https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf',
+					accessoryType: 'pdf',
+					createTime: '2020-10-09 12:30',
+					num: 66.66
+				},
+				{
+					id: 2,
+					userAvatar: 'https://s1.ax1x.com/2020/10/11/0cxdsg.jpg',
+					userName: '啦啦',
+					title: '《饮中八仙歌》',
+					accessoryName: '饮中八仙歌',
+					accessoryLink: 'https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf',
+					accessoryType: 'pdf',
+					createTime: '2020-10-09 12:30',
+					num: 66.66
+				},
+				{
+					id: 3,
+					userAvatar: 'https://s1.ax1x.com/2020/10/11/0cxdsg.jpg',
+					userName: '张三',
+					title: '《饮中八仙歌》',
+					accessoryName: '饮中八仙歌',
+					accessoryLink: 'https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf',
+					accessoryType: 'pdf',
+					createTime: '2020-10-09 12:30',
+					num: 66.66
+				}
+			]
 		};
 	},
 	methods: {
@@ -107,9 +166,9 @@ export default {
 		}
 	},
 	onShow() {
-		this.fetchBanner(); // 获取banner
-		this.fetchFieryShare(); // 获取最热分享
-		this.fetchNewShare(); // 获取最新分享
+		// this.fetchBanner(); // 获取banner
+		// this.fetchFieryShare(); // 获取最热分享
+		// this.fetchNewShare(); // 获取最新分享
 	}
 };
 </script>
@@ -218,50 +277,50 @@ export default {
 				background-color: #0094ff;
 			}
 		}
-		.item{
+		.item {
 			margin-bottom: 20rpx;
 			padding: 20rpx;
 			background-color: #fff;
-			.userMsg{
+			.userMsg {
 				display: flex;
 				align-items: center;
 				padding: 0 0 20rpx 20rpx;
 				color: #999;
 				font-size: 20rpx;
-				image{
+				image {
 					margin-right: 20rpx;
 					width: 100rpx;
 					height: 100rpx;
 					border-radius: 50%;
 				}
-				.userName{
+				.userName {
 					color: #333;
 					font-size: 32rpx;
 					font-weight: 600;
 				}
 			}
-			.classify{
+			.classify {
 				display: inline-block;
 				color: #0094ff;
 			}
-			.title{
+			.title {
 				padding: 20rpx;
 			}
-			.accessory{
+			.accessory {
 				display: flex;
 				align-items: center;
 				margin: 10rpx 0;
-				image{
+				image {
 					margin-right: 10rpx;
 					width: 40rpx;
 				}
 			}
-			.readMsg{
+			.readMsg {
 				display: flex;
 				align-items: center;
 				color: #999;
 				font-size: 26rpx;
-				image{
+				image {
 					margin-right: 6rpx;
 					width: 40rpx;
 				}
